8383215ffaa99bd472473b17e19a583752279f4f
Jerinislam05 <jerinislam05@gmail.com>
2024-01-20 19:54:54 +0000

    learn about local storage

---
+++ local-storage.js
@@ -0,0 +1,8 @@
+/* 
+LocalStorage
+1. man paite getitem
+2. man set korte setitem
+3. man remove korte removeitem
+4. man orthat key, value 2ta e string e thake
+5. local storage theke clear korte hole clear value use korte hobe
+*/
